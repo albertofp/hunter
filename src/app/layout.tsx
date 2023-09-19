48811d4616480abfcc792ae94389863156658c58
albertofp <albertopluecker@gmail.com>
2023-09-19 12:47:17 +0200

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-import { SessionProvider } from "next-auth/react";
 import ProviderWrapper from "@/components/ProviderWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
