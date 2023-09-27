63b12b78d558916916d75e110feedf20ec79747b
albertofp <albertopluecker@gmail.com>
2023-09-27 15:28:42 +0200

    fix deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
 "use client";
 import Job from "@/src/components/Job";
 import samples from "@/src/lib/samples.json";
-import { useQuery } from "@tanstack/react-query";
-import { job } from "../types/job";
-import { JSX } from "react";
 
 export default function Board() {
-  const { data } = useQuery(["jobs"], () =>
-    fetch("https://arbeitnow.com/api/job-board-api/").then((res) =>
-      res.json().then((data) => console.log(data)),
-    ),
-  );
-
   return (
     <div className="h-full w-screen bg-slate-100 pb-6">
       <div className="flex flex-col items-center gap-6 h-full pt-5">
         <h1 className="text-4xl text-slate-600 font-bold">Job Board</h1>
-        {data?.data?.map((job: JSX.IntrinsicAttributes & job) => (
+        {samples.data.map((job) => (
           <Job {...job} key={job.id} />
         ))}
       </div>
